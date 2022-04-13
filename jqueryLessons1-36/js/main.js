2d83f19799add7599c691dfa96811e7ab698a842
Oleksandr <68132542+Hopatsa-Oleksandr@users.noreply.github.com>
2022-04-13 07:59:28 +0300

    Добавил часть практики 2

--- jqueryLessons1-36/js/main.js
+++ jqueryLessons1-36/js/main.js
@@ -1,11 +1,92 @@
-$('img[height=150]');
-$('img[src^=img/]'); //для начала строки
-$('img[src$=.png]'); //для конца строки
+//ниже первоначальный код с которого все начинаеться
+
+$(document).ready(function(){
+   //весь код
+});
+//сокращение 
+$(function(){
+   //весь код
+});
+// расмотрим 3 метода text hide show
+
+// $(function(){
+//    // var tagP = $('.mainText p').text('Новый текст для тега p');
+//    // alert(tagP);
+//    var tagP = $('.mainText p').hide(3000); //скритые елемента
+//    $('.mainText p').show(3000); //показ елемента
+// });
 
 
-$('nav menu li'); //вложенные елементы 
-$('.icon > div'); //дочерние елементы 
-$('.icon img + h4'); //соседние елементы 
+//сокращение кода выше
+
+// $(function(){
+//    // var tagP = $('.mainText p').text('Новый текст для тега p');
+//    // alert(tagP);
+//    var tagP = $('.mainText p'); //скритые елемента
+//    tagP.hide().text('Новый текст').show(4000); //показ елемента
+// });
+
+
+//автоматические цыклы
+
+$(function(){
+$('.icons > div').hide(3000).show(5000);  //выбираем все елементы div в icons [div,div,div,div]
+});
+
+//высота и ширина любого обьекта
+
+// $(function(){
+//    var w = $('.btn').width();
+//      alert(w);
+//      var h = $('.btn').height();
+//      alert(h);
+//    });
+
+// $(function(){
+//    var w = $('.mainText').width(350);
+//      var h = $('.mainText').height(250);
+//    });
+
+$(function(){
+   function widthAndHeight(element) {
+      var className="."+element;
+      var w = $(className).width();
+      var h = $(className).height();
+      $('.mainText p').text('Ширина: '+w+'; Высота: '+h)
+   }
+   widthAndHeight('btn');
+})
+
+
+
+//Примеры как выбирать файлы по атрибутам
+//фильтрация 35.09
+
+$('menu li:even'); //Для не четных
+$('menu li:odd'); //Для четных
+$('img:not(.logo img)');
+$('li:has(a)');
+$('p:contains(client)'); //Выбираем конкретное слово со всеми тегами 'p' в котором есть указанное слово
+$('.logo li:first'); //Первый елемент
+$('#h2 > div + li:last'); //Последний елемент
+$('div:hidden'); // все скрытые елементы
+$('div:visibility'); // все видимые елементы
+
+https://www.youtube.com/watch?v=9Pqf_AIT2ZI
+
+
+
+
+
+// $('img[height=150]');
+// $('img[src^=img/]'); //для начала строки
+// $('img[src$=.png]'); //для конца строки
+// $('a[href*=#]') //в любом месте
+
+
+// $('nav menu li'); //вложенные елементы 
+// $('.icon > div'); //дочерние елементы 
+// $('.icon img + h4'); //соседние елементы 
 
 
 
